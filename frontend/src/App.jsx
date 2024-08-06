552459b7099a4e9f79b3f1d4b49899cda82a33b1
WilsonLe <leanhminh2907@gmail.com>
2024-08-06 10:29:13 -0400

    implement excalidraw wrapper

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+import "./App.css";
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Excalidraw />
+    </div>
+  );
+}
+
+export default App;
--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 	"github.com/wilsonle/excalidraw-collab/controllers"
 	"github.com/wilsonle/excalidraw-collab/middleware"
 	"github.com/wilsonle/excalidraw-collab/pkg/database"
-	"github.com/wilsonle/excalidraw-collab/views"
 )
 
 func main() {
 	database.InitDB("./excalidraw-collab.db")
 
-	http.Handle("/", middleware.BasicAuth(http.HandlerFunc(views.RootHandler)))
 	http.HandleFunc("/login", controllers.LoginHandler)
+
+	// Serve static files
+	fs := http.FileServer(http.Dir("./frontend/dist"))
+	http.Handle("/", middleware.BasicAuth(fs))
 
 	log.Println("Server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
